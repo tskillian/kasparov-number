0e27bad05c9290998f102df52b0401dcd1dd3df2
Tim Killian <tskillian@gmail.com>
2013-12-06 15:03:11 -0800

    improving the scrape of stats page

--- record_against_rating_buckets_scrape.js
+++ record_against_rating_buckets_scrape.js
@@ -1,12 +1,24 @@
 var page = require('webpage').create();
-var test;
+
+// helper function to go through an array of objects with bucket and wins property, finding
+// the highest bucket with a win
+var 
 
 page.open('http://main.uschess.org/datapage/gamestats.php?memid=12869413', function() {
-  page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-    var test = page.evaluate(function() {
-      return $('.blog').siblings('table').children().children().eq(3).contents().eq(2).text();
-    });
-    console.log(test);
-    phantom.exit();
-  });
+	page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
+		var wins = page.evaluate(function() {
+			var rows = [];
+			$('.blog').siblings().find('tr').each(function(i, element) {
+				var $row = $(element);
+				rows.push({
+					bucket: $row.find('td').eq(0).text(),
+					wins: $row.find('td').eq(2).text()
+				});
+			});
+			return rows;
+		});
+		console.log(wins);
+		phantom.exit();
+	});
 });
+
