58efafedc50ed8c146152dcfc7f534e9bbe0302b
Tim Killian <tskillian@gmail.com>
2013-12-06 16:39:03 -0800

    Finished scrape of player stats page. As of right now it just returns the highest rating bucket with a win

--- record_against_rating_buckets_scrape.js
+++ record_against_rating_buckets_scrape.js
@@ -1,12 +1,27 @@
 var page = require('webpage').create();
 
-// helper function to go through an array of objects with bucket and wins property, finding
-// the highest bucket with a win
-var 
+
 
 page.open('http://main.uschess.org/datapage/gamestats.php?memid=12869413', function() {
-	page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-		var wins = page.evaluate(function() {
+	'use strict';
+	page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js', function() {
+		var highestBucket = page.evaluate(function() {
+
+			// Helper function to go through an array of objects with bucket and wins property, finding
+			// the highest bucket with a win
+			var highestBucketWithWin = function(arr) {
+				var topBucket = 0;
+				arr.forEach(function(element) {
+					console.log('topBucket: '+topBucket+' current loop bucket: '+element.bucket);
+					if (parseInt(element.wins, 10) > 0 && parseInt(element.bucket, 10) > topBucket) {
+						topBucket = element.bucket;
+					}
+				});
+				return topBucket;
+			};
+
+			// Populate an array of objects where each object has a bucket and wins property,
+			// the bucket being a given rating and wind being how many wins against that rating
 			var rows = [];
 			$('.blog').siblings().find('tr').each(function(i, element) {
 				var $row = $(element);
@@ -15,9 +30,10 @@ page.open('http://main.uschess.org/datapage/gamestats.php?memid=12869413', funct
 					wins: $row.find('td').eq(2).text()
 				});
 			});
-			return rows;
+			
+			return highestBucketWithWin(rows);
 		});
-		console.log(wins);
+		console.log(highestBucket);
 		phantom.exit();
 	});
 });
