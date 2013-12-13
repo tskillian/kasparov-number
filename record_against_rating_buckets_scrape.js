696ceece35b8e0635bf2931e273c519301ca2f64
Tim Killian <tskillian@gmail.com>
2013-12-12 17:30:45 -0800

    updated stats page scrape to be able to be run as a node process using the phantomjs-node module

--- record_against_rating_buckets_scrape.js
+++ record_against_rating_buckets_scrape.js
@@ -1,39 +1,47 @@
-var page = require('webpage').create();
+var phantom = require('node-phantom');
 var id = 12528459;
 
-page.open('http://main.uschess.org/datapage/gamestats.php?memid='+id, function() {
-	'use strict';
-	page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js', function() {
-		var highestBucket = page.evaluate(function() {
 
-			// Helper function to go through an array of objects with bucket and wins property, finding
-			// the highest bucket with a win
-			var highestBucketWithWin = function(arr) {
-				var topBucket = 0;
-				arr.forEach(function(element) {
-					console.log('topBucket: '+topBucket+' current loop bucket: '+element.bucket);
-					if (parseInt(element.wins, 10) > 0 && parseInt(element.bucket, 10) > topBucket) {
-						topBucket = element.bucket;
-					}
-				});
-				return topBucket;
-			};
+phantom.create(function(err, ph) {
+    'use strict';
+    return ph.createPage(function(err, page) {
+        return page.open('http://main.uschess.org/datapage/gamestats.php?memid=' + id, function(err, status) {
+            console.log('opened site? ', status);
+            page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function() {
+                //jQuery Loaded.
+                //Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds.
 
-			// Populate an array of objects where each object has a bucket and wins property,
-			// the bucket being a given rating and wind being how many wins against that rating
-			var rows = [];
-			$('.blog').siblings().find('tr').each(function(i, element) {
-				var $row = $(element);
-				rows.push({
-					bucket: $row.find('td').eq(0).text(),
-					wins: $row.find('td').eq(2).text()
-				});
-			});
+                return page.evaluate(function() {
+                    // Helper function to go through an array of objects with bucket and wins property, finding
+                    // the highest bucket with a win
+                    var highestBucketWithWin = function(arr) {
+                        var topBucket = 0;
+                        arr.forEach(function(element) {
+                            console.log('topBucket: ' + topBucket + ' current loop bucket: ' + element.bucket);
+                            if (parseInt(element.wins, 10) > 0 && parseInt(element.bucket, 10) > topBucket) {
+                                topBucket = element.bucket;
+                            }
+                        });
+                        return topBucket;
+                    };
+                    // Populate an array of objects where each object has a bucket and wins property,
+                    // the bucket being a given rating and wind being how many wins against that rating
+                    var rows = [];
+                    $('.blog').siblings().find('tr').each(function(i, element) {
+                        var $row = $(element);
+                        rows.push({
+                            bucket: $row.find('td').eq(0).text(),
+                            wins: $row.find('td').eq(2).text()
+                        });
+                    });
+                    console.log(highestBucketWithWin);
+                    return highestBucketWithWin(rows);
+                }, function(err, result) {
+                    console.log(result);
+                    ph.exit();
+                });
 
-			return highestBucketWithWin(rows);
-		});
-		console.log(highestBucket);
-		phantom.exit();
-	});
+            });
+        });
+    });
 });
-
