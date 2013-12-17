409e4ae8f0fd038a4d51a3b8bfe15a9369596fd2
Aaron Pollack <pollackaaron@gmail.com>
2013-12-17 14:54:19 -0800

    Function only returns array of objects, checks for kamsky must be down after calling it

--- Gamestats_Scrape.js
+++ Gamestats_Scrape.js
@@ -10,55 +10,23 @@ var getWin = function(player, bucket, callback) {
     'use strict';
     var test = null;
     var requestURL = 'http://www.uschess.org/datapage/gamestats.php?memid=' + player + '&dkey='+bucket+'&drill=G';
-    var data = request(requestURL, function(error, response, body) {
+    request(requestURL, function(error, response, body) {
         var LW = [];
         var $ = cheerio.load(body);
-        var getPageData = function() {
-            var $table = $('.blog').siblings().find('tr');
-            var tableLength = $table.length;
-            $table.each(function(i, element) {
-                var $row = $(element);
-                var wl = $row.find('td').eq(7).text();
-                var pID = $row.find('td').eq(4).text();
-                var player = {};
-                player.winLoss = wl;
-                player.playerID = pID.trim();
-                LW.push(player);
+        var $table = $('.blog').siblings().find('tr');
+        var tableLength = $table.length;
+        $table.each(function(i, element) {
+            var $row = $(element);
+            var wl = $row.find('td').eq(7).text();
+            var pID = $row.find('td').eq(4).text();
+            var player = {};
+            player.winLoss = wl;
+            player.playerID = pID.trim();
+            LW.push(player);
             });
-            return LW;
         };
-        var checkPlayer = function(playerList) {
-            function isKamksy(array) {
-                for (var i = 0; i < array.length; i++) {
-                    if (array[i]['playerID'] === '12528459' && array[i]['winLoss'] === 'W') { //if player beat Kamsky, return true
-                        return true;
-                    }
-                }
-                return false;
-            };
-
-            function highestWin(array) {
-                for (var i = 0; i < array.length; i++) {
-                    if (array[i]['winLoss'] === 'W') {
-                        return array[i]['playerID']; //return player object of first win
-                    }
-                }
-                return false;
-                
-            };
-
-            var pageData = playerList();
-            if (isKamksy(pageData)) {
-                //callback(true);
-                return true;
-            } else {
-                //return callback(highestWin(pageData));
-                return highestWin(pageData);
-            }
-        };
-        return callback(checkPlayer(getPageData));
-    });
-    //return data ;
+        return callback(LW);
+    );
 };
 
 module.exports = getWin;
