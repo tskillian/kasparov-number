9de0746f21e09963f4284221820fa83954b5f9f4
Smita Busar <smitabusar@gmail.com>
2013-12-18 14:54:04 -0800

    added test cases for all scrapes

--- js/testScrape.js
+++ js/testScrape.js
@@ -1,64 +1,40 @@
-//var uscfId = 12528459; //12870732
-//var uscfId=12869413;
-
 var Scrape = require("./scrape.js")
 
-Scrape.getProfile('12710197', function(error, data) {
+var testuscfID = ['12528459', '12710197', '12870732', '12869413', '1271019'];
+
+for (var i = 0; i < testuscfID.length; i++) {
+    Scrape.getProfile(testuscfID[i], function(error, data) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(data);
+        }
+    });
+    Scrape.getHighestBucketWithWin(testuscfID[i], function(error, data) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(data);
+        }
+    });
+    Scrape.getAllGamesByWinLossDraw(testuscfID[i], 2700, "W", function(error, data) {
+        console.log(error);
+        console.log(data);
+    });
+}
+
+Scrape.getAllGamesByWinLossDraw('12625186', 2800, "W", function(error, data) {
     console.log(error);
     console.log(data);
 });
 
-Scrape.getProfile('1271019', function(error, data) {
+Scrape.getAllGamesByWinLossDraw('12625186', 2700, "L", function(error, data) {
     console.log(error);
     console.log(data);
 });
 
-Scrape.getProfile('', function(error, data) {
+Scrape.getAllGamesByWinLossDraw('12625186', 2700, "D", function(error, data) {
     console.log(error);
     console.log(data);
 });
 
-Scrape.getHighestBucketWithWin('12710197', function(error, data) {
-    console.log(error);
-    console.log(data);
-});
-
-Scrape.getHighestBucketWithWin('1271019', function(error, data) {
-    console.log(error);
-    console.log(data);
-});
-
-Scrape.getHighestBucketWithWin('', function(error, data) {
-    console.log(error);
-    console.log(data);
-});
-
-Scrape.getAllGamesByWinLossDraw('12625186', 2700, "W",function(error, data) {
-    console.log(error);
-    console.log(data);
-});
-
-Scrape.getAllGamesByWinLossDraw('12625186', 2800, "W",function(error, data) {
-    console.log(error);
-    console.log(data);
-});
-
-Scrape.getAllGamesByWinLossDraw('12625186', 2700, "L",function(error, data) {
-    console.log(error);
-    console.log(data);
-});
-
-Scrape.getAllGamesByWinLossDraw('12625186', 2700, "D",function(error, data) {
-    console.log(error);
-    console.log(data);
-});
-
-Scrape.getProfile('1271019', function(error, data) {
-    console.log(error);
-    console.log(data);
-});
-
-Scrape.getProfile('', function(error, data) {
-    console.log(error);
-    console.log(data);
-});
