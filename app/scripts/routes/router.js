e42388bad5380be6f2a89d48ca86387c72248537
Tim Killian <tskillian@gmail.com>
2013-12-19 18:28:22 -0800

    added lines connecting nodes on map

--- app/scripts/routes/router.js
+++ app/scripts/routes/router.js
@@ -21,13 +21,15 @@ KasparovNumber.Routers = KasparovNumber.Routers || {};
                     var path = KasparovNumber.pathCollection;
                     KasparovNumber.graphView = new KasparovNumber.Views.MapGraphView({
                         collection: path
-                    })
+                    });
                     $('#jumpsTitle').css('display', 'block');
                     $('#GraphView').append(KasparovNumber.graphView.render().el);
                     var url;
                     var locationData = {};
+                    var lineCoordinates = [];
                     var data = path.toJSON();
                     console.log(data);
+
                     function initialize() {
                         var mapOptions = {
                             center: new google.maps.LatLng(41.8780025, -93.097702),
@@ -44,30 +46,46 @@ KasparovNumber.Routers = KasparovNumber.Routers || {};
                                 success: function(data) {
                                     console.log(data.results[0].geometry.location);
                                     locationData[element.State] = data.results[0].geometry.location;
+                                    lineCoordinates.push(new google.maps.LatLng(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng));
                                     var marker = new google.maps.Marker({
                                         position: new google.maps.LatLng(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng),
                                         map: map,
                                         title: element.name
                                     });
-                                var message = ("<p>Name: " + element.name + '<br>' +
-                                				"Regular Rating: " + element.RegularRating + '<br>' +
-                                				"Overall Ranking: " + element.OverallRanking + '<br>' +
-                                				"State: " + element.State + "</p>")
-                                addInfoWindow(marker, message);
+                                    var message = ("<p>Name: " + element.name + '<br>' +
+                                        "Regular Rating: " + element.RegularRating + '<br>' +
+                                        "Overall Ranking: " + element.OverallRanking + '<br>' +
+                                        "State: " + element.State + "</p>")
+                                    addInfoWindow(marker, message);
+
+                                },
+                                complete: function() {
+                                    var lines = new google.maps.Polyline({
+                                        path: lineCoordinates,
+                                        geodesic: false,
+                                        strokeColor: '#FF0000',
+                                        strokeOpacity: 1.0,
+                                        strokeWeight: 2
+                                    });
+
+                                    lines.setMap(map);
                                 }
                             });
                         });
-						function addInfoWindow(marker, message) {
-							var info = message;
 
-							var infoWindow = new google.maps.InfoWindow({
-								content: message
-							});
 
-							google.maps.event.addListener(marker, 'click', function() {
-								infoWindow.open(map, marker);
-							});
-						}
+
+                        function addInfoWindow(marker, message) {
+                            var info = message;
+
+                            var infoWindow = new google.maps.InfoWindow({
+                                content: message
+                            });
+
+                            google.maps.event.addListener(marker, 'click', function() {
+                                infoWindow.open(map, marker);
+                            });
+                        }
                     };
 
                     initialize();
