f1e86efe41ebcde45c96c02e5363bcc6af7393e1
Tim Killian <tskillian@gmail.com>
2014-01-03 12:36:48 -0600

    claned up some dead code, updated gitignore

--- js/mongodb_tests.js
+++
@@ -1,6 +0,0 @@
-var mongodb = require('mongodb'),
-	MongoClient = mongodb.MongoClient;
-
-MongoClient.connect('mongodb://heroku:0d2b9433a5dc0f6bacd6bdc8ed1e7e2f@linus.mongohq.com:10010/app20265546', function(err, db) {
-	console.log(db.collection('players'));
-});
